7f5661b12202914d6fb9a6fe975ac952587bf57b
alexandraep <alexandra.pirjol14@gmail.com>
2022-09-28 09:11:35 +0100

    added example of form validation

--- public/crud.js
+++ public/crud.js
@@ -1,4 +1,4 @@
-function readAllSongs(){
+function readAllSongs() {
     fetch(`http://localhost:3000/songs/all`)
         .then(res => res.json())
         .then(data => {
@@ -9,8 +9,9 @@ function readAllSongs(){
 
 
 }
+
 // using async-await with fetch
-async function readAllSongs(){
+async function readAllSongs() {
     let res = await fetch(`http://localhost:3000/songs/all`)
     let data = await res.json()
 
@@ -20,9 +21,10 @@ async function readAllSongs(){
 
 
 }
-window.onload = readAllSongs;
 
-function onReadSong(){
+window.addEventListener("load", readAllSongs);
+
+function onReadSong() {
 
     let input = document.getElementById('songIndex')
     let songIndex = input.value;
@@ -37,19 +39,57 @@ function onReadSong(){
         })
 }
 
-function onUpdateSong() {
+function isFormValid(form) {
+    for (let field of form.querySelectorAll('input,textarea')) {
+        if (field.checkValidity() === false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function setValidityMessage(input, message) {
+    input.addEventListener('input', e => {
+        if (!input.validity.valid) {
+            input.setCustomValidity(message);
+            input.reportValidity();
+        } else {
+            input.setCustomValidity('')
+        }
+    })
+
+}
+
+window.addEventListener('load', e => {
+    let index = document.getElementById('index')
+    let title = document.getElementById('title')
+    let genre = document.getElementById('genre')
+
+    setValidityMessage(index, 'required')
+    setValidityMessage(title, 'required')
+    setValidityMessage(genre, 'At least 3 characters for the genre')
+})
+
+async function onUpdateSong() {
     console.log('submitting')
     let updateForm = document.getElementById('updateForm')
-    let formData = new FormData(updateForm)
-    let data = new URLSearchParams(formData)
+    if (isFormValid(updateForm)) {
+        let formData = new FormData(updateForm)
+        let requestData = new URLSearchParams(formData)
 
-    fetch(`http://localhost:3000/songs/update/${formData.get('index')}`,
-        {
-            method: 'PUT',
-            body: data
-        })
-        .then(res => res.text())
-        .then(text => console.log(text))
+        console.log()
+
+        let res = await fetch(
+            `http://localhost:3000/songs/update/${formData.get('index')}`,
+            {
+                method: 'PUT',
+                body: requestData
+            })
+        let responseData = await res.text();
+        console.log(responseData)
+    } else {
+        alert('The form does not validate, so you cannot send the data')
+    }
 }
 
 function onDeleteSong() {
